6c79764018767b82dce509502ed23fda2c41b4a5
Ayan726 <ayang8702@gmail.com>
2023-12-21 19:41:40 +0530

    axios update

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://cg-assignment-server.onrender.com/";
+const baseURL = "https://cg-assignment-server.onrender.com";
 
 const api = axios.create({
   baseURL,
